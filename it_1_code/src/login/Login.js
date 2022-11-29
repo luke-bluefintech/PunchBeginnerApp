84c60cb8dc3ded3b34265d320e9e09c32f80b7cc
Luke Foley <luke@bluefintechpartners.com>
2022-11-29 18:43:31 -0500

    Login Fixes

--- it_1_code/src/login/Login.js
+++ it_1_code/src/login/Login.js
@@ -7,14 +7,12 @@ function Login() {
             <input type="text" placeholder="Enter Username:" name="uname" required />
 
             <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password:" name="psw" required>
-
-                <button type="submit">Login</button>
-                <label>
-                    <input type="checkbox" checked="checked" name="remember"> Remember me
-                    </input>
-                </label>
-            </input>
+            <input type="password" placeholder="Enter Password:" name="psw" required />
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" name="remember" />
+                <a>Remember me?</a>
+            </label>
         </div>
     )
 }
