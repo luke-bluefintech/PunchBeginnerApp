59682d851c6a83f53936b87d9c882eea88aae49a
Lili Loughlin <lmloughlin@wpi.edu>
2022-11-29 18:21:32 -0500

    added basic code to login

--- it_1_code/src/login/Login.js
+++ it_1_code/src/login/Login.js
@@ -1,5 +1,22 @@
+import './Login.css';
+
 function Login() {
-    return <h1>Login</h1>;
+    return (
+        <div className="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username:" name="uname" required />
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password:" name="psw" required>
+
+                <button type="submit">Login</button>
+                <label>
+                    <input type="checkbox" checked="checked" name="remember"> Remember me
+                    </input>
+                </label>
+            </input>
+        </div>
+    )
 }
 
 export default Login;
