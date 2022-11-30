4e13101c1146f1c29e6b6a721c0d957b36e7dd9e
Lili Loughlin <lmloughlin@wpi.edu>
2022-11-30 14:11:34 -0500

    changed buttons

--- it_1_code/src/table/Table.js
+++ it_1_code/src/table/Table.js
@@ -13,13 +13,13 @@ function Table() {
                 <td>Ilana</td>
                 <td>800</td>
                 <td>546</td>
-                <td><button type="pledge">Pledge</button></td>
+                <td><pledgebutton type="pledge">Pledge</pledgebutton></td>
             </tr>
             <tr>
                 <td>Luke</td>
                 <td>1200</td>
                 <td>753</td>
-                <td><button type="pledge">Pledge</button></td>
+                <td><pledgebutton type="pledge">Pledge</pledgebutton></td>
             </tr>
         </table>
     )
