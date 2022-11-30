3d49d76efab7fc79c4ca7152dbf09ae24bc5edb5
Luke Foley <luke@bluefintechpartners.com>
2022-11-30 16:45:12 -0500

    showing certain react elements

--- it_1_code/src/layout/Layout.js
+++ it_1_code/src/layout/Layout.js
@@ -6,13 +6,19 @@ import ViewProject from "../viewproject/ViewProject";
 import Pledge from "../pledge/Pledge";
 
 const Layout = () => {
+
+    var showLogin = true;
+    var showDashboard = false;
+    var showViewProject = false;
+    var showPledge = false;
+
     return (
         <div>
             <Navbar />
-            <Login />
-            <Dashboard />
-            <ViewProject />
-            <Pledge />
+            {showLogin ? <Login /> : null}
+            {showDashboard ? <Dashboard /> : null}
+            {showViewProject ? <ViewProject /> : null}
+            {showPledge ? <Pledge /> : null}
         </div>
     )
 };
