b7cbc29ef4863e14cab22b9dccb0f8692b544b77
Lili Loughlin <lmloughlin@wpi.edu>
2022-11-30 19:56:01 -0500

    changed text

--- it_1_code/src/newproject/NewProject.js
+++ it_1_code/src/newproject/NewProject.js
@@ -5,22 +5,22 @@ function NewProject() {
     return (
         <div className="container">
             <label for="pname"><b>Project Name:</b><br></br></label>
-            <input type="shorttext" name="pname" required />
+            <input type="text" name="pname" required />
 
             <label for="story"><b><br></br>Your Story:</b><br></br></label>
-            <input type="longtext" name="story" required />
+            <input type="text" name="story" required />
 
             <label for="yname"><b><br></br>Your Name:</b><br></br></label>
-            <input type="shorttext" name="yname" required />
+            <input type="text" name="yname" required />
 
             <label for="genre"><b><br></br>Genre:<br></br></b></label>
-            <input type="shorttext" name="genre" required />
+            <input type="text" name="genre" required />
 
             <label for="goal"><b><br></br>Goal:<br></br></b></label>
-            <input type="shorttext" name="goal" required />
+            <input type="text" name="goal" required />
 
             <label for="deadline"><b><br></br>Deadline:<br></br></b></label>
-            <input type="shorttext" name="deadline" required />
+            <input type="text" name="deadline" required />
 
             <label for="space"><b><br></br><br></br></b></label>
             <createbutton type="create">Create</createbutton>
